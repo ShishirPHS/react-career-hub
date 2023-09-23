ac08e2839a0c581b0c37c16a2d4d9bd26139edb3
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-09-24 00:15:47 +0600

    added show all and see less button

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,9 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState([]);
 
+  // This is not the best way to load show data
+  const [dataLength, setDataLength] = useState(4);
+
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
@@ -14,9 +17,23 @@ const FeaturedJobs = () => {
     <div>
       <h2 className="text-4xl text-center">Featured Jobs {jobs.length}</h2>
       <div className="grid grid-cols-2 gap-6">
-        {jobs.map((job) => (
+        {jobs.slice(0, dataLength).map((job) => (
           <Job key={job.id} job={job}></Job>
         ))}
+      </div>
+      <div className="text-center">
+        <button
+          className={`btn text-2xl ${dataLength === jobs.length && "hidden"}`}
+          onClick={() => setDataLength(jobs.length)}
+        >
+          Show All
+        </button>
+        <button
+          className={`btn text-2xl ${dataLength === jobs.length || "hidden"}`}
+          onClick={() => setDataLength(4)}
+        >
+          See less
+        </button>
       </div>
     </div>
   );
